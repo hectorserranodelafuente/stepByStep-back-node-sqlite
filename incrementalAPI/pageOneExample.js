724c41c94f3b60bb0a80a32b3eab4c0fd4b5bda1
hector <hserranodelafuente@gmail.com>
2025-06-26 18:00:05 +0200

    IA pageOneExample

---
+++ incrementalAPI/pageOneExample.js
@@ -0,0 +1,55 @@
+const env = require('../../env.js')
+// let sqlite3 = require('sqlite3').verbose();
+
+import { GoogleGenAI,Type } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "" });
+
+const schemaYogaDisciplines = {
+  description:"List of different Yoga disciplines",
+  type:Type.ARRAY,
+  items:{
+    type:Type.OBJECT,
+    properties:{
+      title:{
+        type:Type.STRING,
+        description:"Name of the discipline",
+        nullable:false
+      },
+      description:{
+        type:Type.STRING,
+        description:"Description of the discipline in no more than 20 lines",
+        nullable:false
+      }
+    },
+    required:["title","description"]
+  }
+}
+
+async function main() {
+  
+    const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Give me a list of 10 displines derivated from YOGA",
+    config:{
+    responseMimeType:"application/json",
+    responseSchema: schemaYogaDisciplines
+  }
+  });
+  
+  return response
+}
+
+
+
+
+
+class pageOneExample{
+    
+    constructor(){}
+
+    yogaDisciplines(req){    
+        return main().response
+    }
+
+}
